331d8e801e397cb5c061a125c051aad0e2ee71e4
Daniel235-web <samueledaniel235@gmail.com>
2023-12-19 14:07:14 +0000

    navitems

---
+++ src/config/index.ts
@@ -0,0 +1,45 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: 'UI kits',
+        value: 'ui_kits' as const,
+        features: [
+            {
+                name: 'Editor picks',
+                href: '#',
+                imageSrc: '/nav/ui-kits/mixed.jpg',
+            },
+            {
+                name: 'New Arrivals',
+                href: '#',
+                imageSrc: '/nav/ui-kits/purple.jpg',
+            },
+            {
+                name: 'Bestsellers',
+                href: '#',
+                imageSrc: '/nav/ui-kits/blue.jpg',
+            },
+        ],
+    },
+    {
+        label: 'Icons',
+        value: 'icons' as const,
+        features: [
+            {
+                name: 'Favorite icon picks',
+                href: '#',
+                imageSrc: '/nav/icons/picks.jpg',
+            },
+            {
+                name: 'New Arrivals',
+                href: '#',
+                imageSrc: '/nav/icons/new.jpg',
+            },
+            {
+                name: 'Bestselling Icons',
+                href: '#',
+                imageSrc: '/nav/icons/bestsellers.jpg',
+            },
+        ],
+    },
+    
+]
